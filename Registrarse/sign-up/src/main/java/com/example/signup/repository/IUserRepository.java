8415498617c67943741c56b960703071159e08c3
Carlos Becerra <carlosbecerra.dev@gmail.com>
2021-01-03 19:18:26 -0500

    :pencil: Anotación @Repository.

--- Registrarse/sign-up/src/main/java/com/example/signup/repository/IUserRepository.java
+++ Registrarse/sign-up/src/main/java/com/example/signup/repository/IUserRepository.java
@@ -1,9 +1,11 @@
 package com.example.signup.repository;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import com.example.signup.entity.User;
 
+@Repository
 public interface IUserRepository extends CrudRepository<User, Long> {
 
 }
