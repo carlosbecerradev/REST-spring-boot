63387fd96098689058ff96852de3d86a9cf4f70b
Carlos Becerra <carlosbecerra.dev@gmail.com>
2021-01-05 17:58:50 -0500

    :tada: Iniciando el proyecto de Registrar una cuenta de usuario (Sign Up) con correo de confirmación.

---
+++ Registrarse/sign-up-email-confirmation/src/test/java/com/example/app/SignUpEmailConfirmationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SignUpEmailConfirmationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
