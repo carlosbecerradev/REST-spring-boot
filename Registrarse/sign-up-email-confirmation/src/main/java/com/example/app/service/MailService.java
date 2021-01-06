c3bdc344fe722780c3f07a0066a5640e1e704f09
Carlos Becerra <carlosbecerra.dev@gmail.com>
2021-01-06 18:24:07 -0500

    :heavy_plus_sign: Servicio para enviar la notificaćión al correo.

---
+++ Registrarse/sign-up-email-confirmation/src/main/java/com/example/app/service/MailService.java
@@ -0,0 +1,33 @@
+package com.example.app.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.mail.javamail.MimeMessagePreparator;
+import org.springframework.stereotype.Service;
+
+import com.example.app.pojo.NotificationEmail;
+
+@Service
+public class MailService {
+
+	@Autowired
+	private JavaMailSender javaMailSender;
+
+	public void sendMail(NotificationEmail notificationEmail) {
+		MimeMessagePreparator messagePreparator = mimeMessage -> {
+			MimeMessageHelper messageHelper = new MimeMessageHelper(mimeMessage);
+			messageHelper.setFrom("cinewolke@gmail.com");
+			messageHelper.setTo(notificationEmail.getRecipient());
+			messageHelper.setSubject(notificationEmail.getSubject());
+			messageHelper.setText(notificationEmail.getBody());			
+		};
+		
+		try {
+			javaMailSender.send(messagePreparator);
+			System.out.println("Email confirmation sent!!");
+		} catch (Exception e) {
+			throw new RuntimeException("Exception occurred when sending mail to " + notificationEmail.getRecipient());
+		}
+	}
+}
