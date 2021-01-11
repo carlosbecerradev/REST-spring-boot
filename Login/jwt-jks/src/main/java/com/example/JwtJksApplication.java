6515ac62395f7d4e877643db219ae6c487020fbf
Carlos Becerra <carlosbecerra.dev@gmail.com>
2021-01-11 18:24:14 -0500

    :rocket: Mostrando las llaves.

--- Login/jwt-jks/src/main/java/com/example/JwtJksApplication.java
+++ Login/jwt-jks/src/main/java/com/example/JwtJksApplication.java
@@ -1,13 +1,27 @@
 package com.example;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.example.service.JKSService;
+
 @SpringBootApplication
-public class JwtJksApplication {
+public class JwtJksApplication implements CommandLineRunner {
+	
+	@Autowired
+	private JKSService jksService;
 
 	public static void main(String[] args) {
 		SpringApplication.run(JwtJksApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		// TODO Auto-generated method stub
+		System.out.println("public: " + jksService.getPublicKey());
+		System.out.println("private: " + jksService.getPrivateKey());		
 	}
 
 }
