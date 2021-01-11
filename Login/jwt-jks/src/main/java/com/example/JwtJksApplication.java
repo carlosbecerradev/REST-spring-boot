11999f5fac815bf868f362bbadf57e2356e3b1ad
Carlos Becerra <carlosbecerra.dev@gmail.com>
2021-01-10 19:24:44 -0500

    :tada: Proyecto para generar un JWT usando un JKS.

---
+++ Login/jwt-jks/src/main/java/com/example/JwtJksApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtJksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwtJksApplication.class, args);
+	}
+
+}
