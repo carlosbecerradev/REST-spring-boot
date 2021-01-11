4e652d37d9ff973b30d93f54221535285edcd727
Carlos Becerra <carlosbecerra.dev@gmail.com>
2021-01-11 18:38:07 -0500

    :rocket: Publicando 'JWT' y 'Claims' generados utilizando un archivo 'JKS'.

--- Login/jwt-jks/src/main/java/com/example/JwtJksApplication.java
+++ Login/jwt-jks/src/main/java/com/example/JwtJksApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.example.service.JJWTService;
 import com.example.service.JKSService;
 
 @SpringBootApplication
@@ -12,6 +13,8 @@ public class JwtJksApplication implements CommandLineRunner {
 	
 	@Autowired
 	private JKSService jksService;
+	@Autowired
+	private JJWTService jjwtService;
 
 	public static void main(String[] args) {
 		SpringApplication.run(JwtJksApplication.class, args);
@@ -20,8 +23,12 @@ public class JwtJksApplication implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 		// TODO Auto-generated method stub
-		System.out.println("public: " + jksService.getPublicKey());
-		System.out.println("private: " + jksService.getPrivateKey());		
+		System.out.println("Public key: " + jksService.getPublicKey());
+		System.out.println("Private key: " + jksService.getPrivateKey());	
+		
+		String jwt = jjwtService.generateToken("secret");
+		System.out.println("JWT: " + jwt);	
+		System.out.println("Claims: " + jjwtService.getClaims(jwt));		
 	}
 
 }
